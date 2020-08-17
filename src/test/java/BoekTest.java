05bf6d47aca88497b6f3b377c9f6647e956968e7
Guido1956 <GuidoDulos@valori.nl>
2020-08-17 15:43:45 +0200

    Versie 1.2

--- src/test/java/BoekTest.java
+++ src/test/java/BoekTest.java
@@ -17,7 +17,7 @@ public class BoekTest {
         // test1
         String expectedResult = "Dit is een mooie titel";
         assertEquals(expectedResult, boek.getTitle());
-        System.out.println("hoi againdd");
+        System.out.println("hoi againd");
 
     }
 }
